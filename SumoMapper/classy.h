a6336e264a6785d732ce95494876832fa62e16a1
Oscar C <oscar.coghlan@gmail.com>
2020-10-21 20:22:02 +1100

    Moved functions into header file

--- SumoMapper/classy.h
+++ SumoMapper/classy.h
@@ -1,3 +1,9 @@
+//These libraries are needed for the script to function
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 //Player object
 class Player {
     public:
@@ -18,6 +24,7 @@ class Detection {
 public:
     double distance, direction;
     Detection(double, double);
+    void SetValues(double, double);
     double absDir (double oDir) {return direction + oDir;};
     double x (double playerOffsetX = 0, double oDir = 0) {return (distance * cos((direction + oDir) * (M_PI/180))) + playerOffsetX;};
     double y (double playerOffsetY = 0, double oDir = 0) {return (distance * sin((direction + oDir) * (M_PI/180))) + playerOffsetY;};
@@ -25,6 +32,41 @@ public:
 };
 
 Detection::Detection (double a, double b) {
-        distance = a;
-        direction = b;
+    distance = a;
+    direction = b;
 }
+
+void Detection::SetValues (double a, double b) {
+    distance = a;
+    direction = b;
+}
+
+//This function prints the current state of the plane to the console, unnessasary in arduino as it is only for human interface
+void printToConsole(bool plane[100][100]) {
+    for(size_t y = 0; y < 100; y++) {
+        for(size_t x = 0; x < 100; x++) {
+            if(plane[x][y]) {
+                std::cout << "00";
+            } else {
+                std::cout << "  ";
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+
+//This function draws a circle, it is also unnessasary in arduino
+int circle(int x, double r, double h, double k, int mod) {
+    int newY = (int)(k + mod * sqrt(pow(r, 2) - pow(x - h, 2)) + 0.5 * mod);
+    if (newY != -2147483648) {
+        return newY;
+    } else {
+        return 0;
+    }
+}
+
+//This function simply checks if a point with coordinates (x,y) falls inside a circle with radius r and a centre at (h,k)
+bool checkIfInsideArena(double x, double y, double r, double h, double k) {
+    return (pow(x - h, 2) + pow(y - k, 2) < pow(r, 2));
+}
+
--- SumoMapper/main.cpp
+++ SumoMapper/main.cpp
@@ -2,47 +2,11 @@
 This code is meant to be both a practical implementation and digital representation of the robot sumo arena.
 */
 
-//These libraries are needed for the script to function
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 //Remove these for arduino
 #include <iostream>
 
 //Le classes
 #include <classy.h>
-
-
-//This function prints the current state of the plane to the console, unnessasary in arduino as it is only for human interface
-void printToConsole(bool plane[100][100]) {
-    for(size_t y = 0; y < 100; y++) {
-        for(size_t x = 0; x < 100; x++) {
-            if(plane[x][y]) {
-                std::cout << "##";
-            } else {
-                std::cout << "  ";
-            }
-        }
-        std::cout << std::endl;
-    }
-}
-
-//This function draws a circle, it is also unnessasary in arduino
-int circle(int x, double r, double h, double k, int mod) {
-    int newY = (int)(k + mod * sqrt(pow(r, 2) - pow(x - h, 2)) + 0.5 * mod);
-    if (newY != -2147483648) {
-        return newY;
-    } else {
-        return 0;
-    }
-}
-
-//This function simply checks if a point with coordinates (x,y) falls inside a circle with radius r and a centre at (h,k)
-bool checkIfInsideArena(double x, double y, double r, double h, double k) {
-    return (pow(x - h, 2) + pow(y - k, 2) < pow(r, 2));
-}
 
 int main()
 {
@@ -60,32 +24,39 @@ int main()
     //Initialise player
     Player self(50, 50, 270);
 
-    //Create fake detection
-    Detection enemy(rand() % 31, rand() % 361);
+    //Create fake detection - testing only
+    //Detection enemy(rand() % 31, rand() % 361);
+
+    //Initialise detection, assumes ultrasonic is already pointing towards enemy
+    Detection enemy(/*ultrasonic detection*/2, /*servo detection*/2);
+    Detection newEnemy(/*ultrasonic detection*/2, /*servo detection*/2);
 
     while (true) {
 
-        //Clear plane data
+        /*
+        newEnemy.SetValues(ultrasonic detection, servo detection)
+        */
+        //Clear plane data - redundant in arduino
         for(size_t y = 0; y < 100; y++) {
             for(size_t x = 0; x < 100; x++) {
                 plane[x][y] = false;
             }
         }
 
-        //Draw the arena
+        //Check if the detected object is inside the arena, if so, set new enemy to the enemy variable. This enemy variable is what the robot uses to make its decisions
+        if (checkIfInsideArena(newEnemy.x(self.x, self.direction), newEnemy.y(self.y, self.direction), radius, h, k)) {
+            //plane[(int)enemy.x(self.x, self.direction)][(int)enemy.y(self.y, self.direction)] = true;
+            enemy = newEnemy;
+        }
+
+
+        //Draw the arena - redundunt in arduino
         for (int x = 0; x < 100; x++) {
             plane[x][circle(x, radius, h, k, 1)] = true;
             plane[x][circle(x, radius, h, k, -1)] = true;
         }
 
-        //Check if the detected object is inside the arena
-        if (checkIfInsideArena(enemy.x(self.x, self.direction), enemy.y(self.y, self.direction), radius, h, k)) {
-            plane[(int)enemy.x(self.x, self.direction)][(int)enemy.y(self.y, self.direction)] = true;
-        }
-
-        //Draw the player, and draw a circle around the player
-        //plane[(int)self.x][(int)self.y] = true;
-
+        //Draw the player, and draw a circle around the player - redundant in arduino
         for (int x = 0; x < 100; x++) {
             plane[x][circle(x, 8, self.x, self.y, 1)] = true;
             plane[x][circle(x, 8, self.x, self.y, -1)] = true;
@@ -94,10 +65,11 @@ int main()
         self.x = self.x + 3*cos(self.rad());
         self.y = self.y + 3*sin(self.rad());
 
-        //Print state of plane to console
+        //Print state of plane to console - redundant in arduino
         system("cls");
         std::cout << "coordinates: " << self.x << ", " << self.y;
         printToConsole(plane);
+
     }
     return 0;
 }
